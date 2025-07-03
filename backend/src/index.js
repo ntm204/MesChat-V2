73cad8975f9076179a9558ab71fc806d5c94254c
Nguyễn Tuấn Minh <nguyentuanminh@MingMyMac.local>
2025-07-03 09:47:47 +0700

    fix: increase image upload limit to 2MB

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
+
 app.use(cookieParser());
 app.use(
   cors({
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,8 @@ export default {
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["light",
+    themes: [
+      "light",
       "dark",
       "cupcake",
       "bumblebee",
